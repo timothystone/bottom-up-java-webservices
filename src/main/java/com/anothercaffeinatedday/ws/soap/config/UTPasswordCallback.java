308e828c1c77029d7f04bceccdc835c45bc92f0a
Timothy Stone <github@petmystone.com>
2021-01-11 21:25:43 -0500

    Set request interceptor properties and callback class
    The Username Token Callback class is stubbed out. The WebServiceConfig
    properties for the request interceptor are created using the
    ConfigurationConstants for plain text password validation.

---
+++ src/main/java/com/anothercaffeinatedday/ws/soap/config/UTPasswordCallback.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.ws.soap.config;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+
+public class UTPasswordCallback implements CallbackHandler {
+    @Override
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
+    }
+}
--- src/main/java/com/anothercaffeinatedday/ws/soap/config/WebServiceConfig.java
+++ src/main/java/com/anothercaffeinatedday/ws/soap/config/WebServiceConfig.java
@@ -4,6 +4,8 @@ import com.anothercaffeinatedday.ws.soap.PaymentProcessorImpl;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +26,10 @@ public class WebServiceConfig {
         endpoint.publish("/paymentProcessor");
 
         Map<String, Object> properties = new HashMap<>();
+
+        properties.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+        properties.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+        properties.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
 
         WSS4JInInterceptor request = new WSS4JInInterceptor(properties);
         endpoint.getInInterceptors().add(request);
