9380f516c1beb1cf9fdfbf4db59561d6ce6bdbac
Timothy Stone <github@petmystone.com>
2021-01-11 21:29:58 -0500

    Implement an in-memory password database
    Define a simple Map of usernames and passwords in the constructor of the
    UTPasswordCallback class.

--- src/main/java/com/anothercaffeinatedday/ws/soap/config/UTPasswordCallback.java
+++ src/main/java/com/anothercaffeinatedday/ws/soap/config/UTPasswordCallback.java
@@ -4,8 +4,18 @@ import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UTPasswordCallback implements CallbackHandler {
+
+    private Map<String,String> passwords = new HashMap<>();
+
+    public UTPasswordCallback() {
+        passwords.put("tstone", "tstone");
+        passwords.put("bella", "bella");
+    }
+    
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
 
