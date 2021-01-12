0073f50bbd05c0c5fb48bfb0a3a084a14cb64c82
Timothy Stone <github@petmystone.com>
2021-01-11 21:36:55 -0500

    Implement the handle method of the callback
    Loop through the callbacks on the interceptor and set the password
    matched from the memory hashmap.

--- src/main/java/com/anothercaffeinatedday/ws/soap/config/UTPasswordCallback.java
+++ src/main/java/com/anothercaffeinatedday/ws/soap/config/UTPasswordCallback.java
@@ -1,5 +1,7 @@
 package com.anothercaffeinatedday.ws.soap.config;
 
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
@@ -18,6 +20,13 @@ public class UTPasswordCallback implements CallbackHandler {
     
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-
+        for (Callback callback: callbacks) {
+            WSPasswordCallback passwordCallback = (WSPasswordCallback) callback;
+            String password = passwords.get(passwordCallback.getIdentifier());
+            if(password != null) {
+                passwordCallback.setPassword(password);
+                return;
+            }
+        }
     }
 }
