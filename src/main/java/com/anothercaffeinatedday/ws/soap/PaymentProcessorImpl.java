9d95f74d75e14eed8ed5ed3a60d392ffe7acade7
Timothy Stone <github@petmystone.com>
2021-01-24 12:46:57 -0500

    Throw RuntimeException to demonstrate soap fault
    The RuntimeException on the length or value of the Credit Number throws
    a Server fault.

--- src/main/java/com/anothercaffeinatedday/ws/soap/PaymentProcessorImpl.java
+++ src/main/java/com/anothercaffeinatedday/ws/soap/PaymentProcessorImpl.java
@@ -5,11 +5,16 @@ import com.anothercaffeinatedday.ws.soap.dto.PaymentProcessorResponse;
 
 public class PaymentProcessorImpl implements PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) {
-		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
-		// Business Logic or a call to a Business Logic Class Goes Here.
-		paymentProcessorResponse.setResult(true);
-		return paymentProcessorResponse;
-	}
+  public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) {
+    PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
+    String creditCardNumber = paymentProcessorRequest.getCreditCardInfo().getCardNumber();
+    // Business Logic or a call to a Business Logic Class Goes Here.
+    if (creditCardNumber == null || creditCardNumber.length() == 0) {
+      throw new RuntimeException("Invalid Card Number");
+    }
+
+    paymentProcessorResponse.setResult(true);
+    return paymentProcessorResponse;
+  }
 
 }
